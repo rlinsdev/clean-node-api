45fd84338f022e09706c2b13579ac98ffa099304
Rodrigo Lins e Freitas <rlinsdev@gmail.com>
2021-02-06 20:57:07 -0300

    Test without httpRequest and with no httpRequest.body

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,12 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email || !httpRequest.body.password) {
+    if (!httpRequest || !httpRequest.body) {
+      return {
+        statusCode: 500
+      }
+    }
+    const { email, password } = httpRequest.body
+    if (!email || !password) {
       return {
         statusCode: 400
       }
@@ -19,9 +25,7 @@ describe('Login router', () => {
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
   })
-})
 
-describe('Login router', () => {
   test('Should return 400 if no password is provider', () => {
     const sut = new LoginRouter() // Suit under Test
     const httpRequest = {
@@ -31,5 +35,19 @@ describe('Login router', () => {
     }
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 500 if no httpRequest was passed', () => {
+    const sut = new LoginRouter() // Suit under Test
+
+    const httpResponse = sut.route()
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Should return 500 if no httpRequest has no Body', () => {
+    const sut = new LoginRouter() // Suit under Test
+    const httpRequest = { }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
   })
 })

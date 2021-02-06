5f9684c6d303c0394894cef2e29d424a5b29aea8
Rodrigo Lins e Freitas <rlinsdev@gmail.com>
2021-02-06 20:31:12 -0300

    Test Email and password

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -9,7 +9,7 @@ class LoginRouter {
 }
 
 describe('Login router', () => {
-  test('Should return 4000 if no email is provider', () => {
+  test('Should return 400 if no password is provider', () => {
     const sut = new LoginRouter() // Suit under Test
     const httpRequest = {
       body: {
@@ -18,6 +18,18 @@ describe('Login router', () => {
     }
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    // expect(httpResponse.statusCode).expect.toBe(400)
+  })
+})
+
+describe('Login router', () => {
+  test('Should return 400 if no password is provider', () => {
+    const sut = new LoginRouter() // Suit under Test
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
   })
 })

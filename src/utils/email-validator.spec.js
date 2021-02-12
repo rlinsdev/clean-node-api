697cb816a06afe29c09d0f29349264415bebf256
Rodrigo Lins e Freitas <rlinsdev@gmail.com>
2021-02-12 08:04:14 -0300

    Make Sut, test the parameter usage

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -5,17 +5,28 @@ class EmailValidator {
   }
 }
 
+const makeSut = () => {
+  return new EmailValidator()
+}
+
 describe('Email Validator', () => {
   test('Should return true if validator returns true', () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('valid_email@mail.com')
     expect(isEmailValid).toBe(true)
   })
 
   test('Should return false if validator returns false', () => {
     validator.isEmailValid = false
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('any_email@mail.com')
     expect(isEmailValid).toBe(false)
+  })
+
+  test('Should call validator with correct email', () => {
+    const sut = makeSut()
+    const anyEmail = 'any_email@mail.com'
+    sut.isValid(anyEmail)
+    expect(validator.email).toBe(anyEmail)
   })
 })

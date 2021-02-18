bc2ccd16126447cb849bc8a5e92286dc0c7a9726
Rodrigo Lins e Freitas <rlinsdev@gmail.com>
2021-02-18 17:46:51 -0300

    ensure Encrypter calls bcryp with correct values

--- src/utils/helpers/encrypter.spec.js
+++ src/utils/helpers/encrypter.spec.js
@@ -8,14 +8,21 @@ class Encrypter {
 describe('Encrypter', () => {
   test('Should return true if bcrypt returns true ', async () => {
     const sut = new Encrypter()
-    const isValid = await sut.compare('any_value', 'hashad_value')
+    const isValid = await sut.compare('any_value', 'hashed_value')
     expect(isValid).toBe(true)
   })
 
   test('Should return false if bcrypt returns false ', async () => {
     const sut = new Encrypter()
     bcrypt.isValid = false
-    const isValid = await sut.compare('any_password', 'hashad_value')
+    const isValid = await sut.compare('any_value', 'hashed_value')
     expect(isValid).toBe(false)
+  })
+
+  test('Should call bcrypt with correct values ', async () => {
+    const sut = new Encrypter()
+    await sut.compare('any_value', 'hashed_value')
+    expect(bcrypt.value).toBe('any_value')
+    expect(bcrypt.hash).toBe('hashed_value')
   })
 })

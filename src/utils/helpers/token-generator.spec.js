1c9ed2b9232ce44eb783fdb204c602774b184d07
Rodrigo Lins e Freitas <rlinsdev@gmail.com>
2021-02-20 17:43:29 -0300

    Injection of secret in token generator

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,20 +1,36 @@
 const jwt = require('jsonwebtoken')
 class TokenGenerator {
+  constructor (secret) {
+    this.secret = secret
+  }
+
   async generate (id) {
-    return jwt.sign(id, 'secret')
+    return jwt.sign(id, this.secret)
   }
 }
+
+const makeSut = () => {
+  return new TokenGenerator('secret')
+}
+
 describe('Token Generator', () => {
   test('Should return null if JWT returns null', async () => {
-    const sut = new TokenGenerator()
+    const sut = makeSut()
     jwt.token = null
     const token = await sut.generate('any_id')
     expect(token).toBeNull()
   })
 
   test('Should return a token if JWT returns token', async () => {
-    const sut = new TokenGenerator()
+    const sut = makeSut()
     const token = await sut.generate('any_id')
     expect(token).toBe(jwt.token)
+  })
+
+  test('Should call jwt with correct values', async () => {
+    const sut = makeSut()
+    await sut.generate('any_id')
+    expect(jwt.id).toBe('any_id')
+    expect(jwt.secret).toBe(sut.secret)
   })
 })

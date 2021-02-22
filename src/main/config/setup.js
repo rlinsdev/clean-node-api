5e93ca17d4c63b44670f2f495379eb9565209887
Rodrigo Lins e Freitas <rlinsdev@gmail.com>
2021-02-22 16:20:04 -0300

    ensure requests parsers body as JSON

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,6 +1,8 @@
 const cors = require('../middleware/cors')
+const JSONParser = require('../middleware/json-parser')
 
 module.exports = app => {
   app.disable('x-powered-by')
   app.use(cors)
+  app.use(JSONParser)
 }
--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -10,15 +10,4 @@ describe('App Setup', () => {
 
     expect(res.headers['x-powered-by']).toBeUndefined()
   })
-
-  test('Should enabled CORS', async () => {
-    app.get('/test_cors', (req, res) => {
-      res.send('')
-    })
-    const res = await request(app).get('/test_cors')
-
-    expect(res.headers['access-controll-allow-origin']).toBe('*')
-    expect(res.headers['access-controll-allow-methods']).toBe('*')
-    expect(res.headers['access-controll-allow-headers']).toBe('*')
-  })
 })

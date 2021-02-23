4968e6a60fa8115c9a8a6a9d924254d8fe84a28f
Rodrigo Lins e Freitas <rlinsdev@gmail.com>
2021-02-23 13:23:50 -0300

    ensure requests are json by default

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,8 +1,10 @@
 const cors = require('../middleware/cors')
 const JSONParser = require('../middleware/json-parser')
+const contentType = require('../middleware/content-type')
 
 module.exports = app => {
   app.disable('x-powered-by')
   app.use(cors)
   app.use(JSONParser)
+  app.use(contentType)
 }

f9ad50cf6f1428691975fd9cad9b1ce53592928b
Rodrigo Lins e Freitas <rlinsdev@gmail.com>
2021-02-26 19:50:40 -0300

    general refactoring

--- src/main/index.js
+++ src/main/index.js
@@ -3,7 +3,7 @@ const env = require('../main/config/env')
 MongoHelper.connect(env.mongoUrl)
   .then(() => {
     const app = require('./config/app')
-    app.listen(5858, () => {
+    app.listen(env.port, () => {
       console.log('server running')
     })
   })
